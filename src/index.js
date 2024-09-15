488502037f9f9889e0ca23c0e0d49acc1c794b13
rizalyuniar <rizalyuniar123@gmail.com>
2024-09-16 03:37:01 +0700

    new commit

--- src/index.js
+++ src/index.js
@@ -11,7 +11,7 @@ const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
     <Provider store = {store}>
-      <BrowserRouter basename='/react/demo'>
+      <BrowserRouter basename='/'>
         <ThemeContext>
           <App />
         </ThemeContext>
